b5ff433ef2aaf5337b8a84464be3b707bfe99bbb
giornogiovana <drakeb2008@hotmail.com>
2021-02-21 15:03:00 -0500

    Project finished

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          alt=""
+          className="header__icon"
+          src="https://d1.awsstatic.com/product-marketing/AI/customers/Customers%20Page/600x400_Airbnb_Logo.f891e0066454456292ad7009aad136ea679f2cfd.png"
+        />
+      </Link>
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+};
